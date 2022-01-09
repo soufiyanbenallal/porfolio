c4609a6e69fc28189ee8363ab4d8cc37bc4c4b47
soufianebenallal <benallalsoufiane1@gmail.com>
2022-01-09 18:19:54 +0000

    config

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  media: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
