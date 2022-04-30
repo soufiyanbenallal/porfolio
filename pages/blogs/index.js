73a114ee0a19254b8328a0252744b52bc22519ed
soufianebenallal <benallalsoufiane1@gmail.com>
2022-04-30 03:11:15 +0000

    add api strapi

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,12 +1,9 @@
-import React from 'react'
+import React , { useEffect,useState } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
-
+import { get } from "../../services/api.service";
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Portfolio', href: '/' },
 ]
 
 const posts = [
@@ -64,61 +61,88 @@ return classes.filter(Boolean).join(' ')
 }
 
 export default function Index() {
-return (
-    <HeroContent>
-    <div className="pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-        <div className="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
-            <div>
-            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Recent publications</h2>
-            <p className="mt-3 text-xl text-gray-500 sm:mt-4">
-                Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus
-                arcu.
-            </p>
-            </div>
-            <div className="mt-12 grid gap-16 pt-12 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
-            {posts.map((post) => (
-                <div key={post.title}>
-                <div>
-                    <a href={post.category.href} className="inline-block">
-                    <span
-                        className={classNames(
-                        post.category.color,
-                        'inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium'
-                        )}
-                    >
-                        {post.category.name}
-                    </span>
-                    </a>
-                </div>
-                <a href={post.href} className="block mt-4">
-                    <p className="text-xl font-semibold text-gray-900">{post.title}</p>
-                    <p className="mt-3 text-base text-gray-500">{post.description}</p>
-                </a>
-                <div className="mt-6 flex items-center">
-                    <div className="flex-shrink-0">
-                    <a href={post.author.href}>
-                        <span className="sr-only">{post.author.name}</span>
-                        <img className="h-10 w-10 rounded-full" src={post.author.imageUrl} alt="" />
-                    </a>
-                    </div>
-                    <div className="ml-3">
-                    <p className="text-sm font-medium text-gray-900">
-                        <a href={post.author.href}>{post.author.name}</a>
-                    </p>
-                    <div className="flex space-x-1 text-sm text-gray-500">
-                        <time dateTime={post.datetime}>{post.date}</time>
-                        <span aria-hidden="true">&middot;</span>
-                        <span>{post.readingTime} read</span>
-                    </div>
-                    </div>
-                </div>
-                </div>
-            ))}
-            </div>
-        </div>
-    </div>
-    </HeroContent>
-)
+  const [articles, setArticles] = useState({meta: {}, data: []});
+
+  /**
+   * fetch articles
+   * @returns {Promise<void>}
+   */
+   const fetchArticles = () => {
+    get('articles')
+    .then(({data}) => {
+        console.log(res);
+        setArticles(data);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+   }
+
+   /**
+    * lifecycle hook 
+    */
+   useEffect(() => {
+    console.log('process.env.API_URL',process.env.NEXT_PUBLIC_ENV_API_URL);
+    fetchArticles()
+     return () => {
+     }
+   }, [])
+   
+  return (
+      <HeroContent>
+      <div className="pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+          <div className="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
+              <div>
+              <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Recent publications</h2>
+              <p className="mt-3 text-xl text-gray-500 sm:mt-4">
+                  Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus
+                  arcu.
+              </p>
+              </div>
+              <div className="mt-12 grid gap-16 pt-12 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
+              {articles.map((post) => (
+                  <div key={post.title}>
+                  <div>
+                      <a href={`${slug}`} className="inline-block">
+                      <span
+                          className={classNames(
+                          post.category.color,
+                          'inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium'
+                          )}
+                      >
+                          {post.category.name}
+                      </span>
+                      </a>
+                  </div>
+                  <a href={post.href} className="block mt-4">
+                      <p className="text-xl font-semibold text-gray-900">{post.title}</p>
+                      <p className="mt-3 text-base text-gray-500">{post.description}</p>
+                  </a>
+                  <div className="mt-6 flex items-center">
+                      <div className="flex-shrink-0">
+                      <a href={post.author.href}>
+                          <span className="sr-only">{post.author.name}</span>
+                          <img className="h-10 w-10 rounded-full" src={post.author.imageUrl} alt="" />
+                      </a>
+                      </div>
+                      <div className="ml-3">
+                      <p className="text-sm font-medium text-gray-900">
+                          <a href={post.author.href}>{post.author.name}</a>
+                      </p>
+                      <div className="flex space-x-1 text-sm text-gray-500">
+                          <time dateTime={post.datetime}>{post.date}</time>
+                          <span aria-hidden="true">&middot;</span>
+                          <span>{post.readingTime} read</span>
+                      </div>
+                      </div>
+                  </div>
+                  </div>
+              ))}
+              </div>
+          </div>
+      </div>
+      </HeroContent>
+  )
 }
 
 function HeroContent({children}) {
---
+++ services/api.service.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+/**
+ * basic api service get
+ * @param {string} url - url to get
+ * @param {object} params - params to send
+ * @returns {Promise<any>}
+ */
+export const get = (url, payload= {}) => {
+    return axios.get(process.env.NEXT_PUBLIC_ENV_API_URL + url , {
+        params : payload
+    });
+}
