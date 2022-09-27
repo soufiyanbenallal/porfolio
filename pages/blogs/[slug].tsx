dfe48631811730d39358afbd6275741605a804c6
SOUFIYAN BENALLAL <41121758+soufiyanbenallal@users.noreply.github.com>
2022-09-27 09:56:41 +0100

    feat: build single post page

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -1,10 +1,85 @@
 import Image from 'next/image';
-import React, { ReactElement } from 'react';
-
+import React, { ReactElement, useEffect, useState } from 'react';
+import { get } from '../../services/api.service';
+import { useRouter } from 'next/router'
 export default function Blog(): ReactElement {
+  const router = useRouter()
+  const {slug} = router.query
+  const [post, setPost] = useState<any>({ meta: {}, data: [] });
+  // const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [procced, setProcced] = useState(false);
+  // const [page] = useState(0);
+  /**
+   * fetch articles
+   * @return {Promise<void>}
+   */
+   const fetchPost = () => {
+
+     const payload = {
+      'key': '589db86ec9016868926b237fcf',
+      page
+      // populate: 'author,category',
+     }
+    get(`content/posts/${slug}`, payload)
+    .then(({data}) => {
+        setPost(data.posts[0]);
+        setTimeout(() => {
+          handleCodeCopy()
+        }, 300);
+    })
+    .catch(err => {
+      console.log(err);
+      
+    })
+   }
+
+  /**
+   * lifecycle hook
+   */
+  useEffect(() => {
+    if (router.isReady) {
+      // Code using query
+      console.log(router.query);
+      fetchPost();
+    }
+    
+  }, [router.isReady]);
+  const handleCodeCopy = ()=> {
+    
+    if (procced) {
+        return;
+      }
+      
+      const pres = document.querySelectorAll('pre') as unknown as HTMLElement[]|| []
+      pres.forEach((pre)=>{
+        if (pre.children.length > 1) {
+          return;
+        }
+        pre.classList.add('relative')
+        const span = document.createElement('span'); // is a node
+        span.className =('absolute top-0 right-0 text-white px-3 py-2 block text-xs cursor-pointer hover:text-red-base active:scale-95')
+        span.innerText = 'Copy';
+        span.onclick = ()=>{
+          span.innerText = 'Copied';
+          const ct = pre.querySelector('code')?.innerText
+          navigator.clipboard.writeText(ct|| '');
+          setTimeout(() => {
+            span.innerText = 'Copy';
+          }, 2000);
+          
+        }
+        pre.appendChild(span);
+        // pre.append('<span class=\'\'>Copy</span>')
+
+      })
+    
+    setProcced(true)
+  }
   return (
-    <div className="relative py-16 bg-white overflow-hidden">
-      <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+    <div className="relative py-32 bg-white overflow-hidden">
+
+      <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full ">
         <div
           className="relative h-full text-lg max-w-prose mx-auto"
           aria-hidden="true"
@@ -107,96 +182,8 @@ export default function Blog(): ReactElement {
           </svg>
         </div>
       </div>
-      <div className="relative px-4 sm:px-6 lg:px-8">
-        <div className="text-lg max-w-prose mx-auto">
-          <h1>
-            <span className="block text-base text-center text-red-base font-semibold tracking-wide uppercase">
-              Introducing
-            </span>
-            <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              JavaScript for Beginners
-            </span>
-          </h1>
-          <p className="mt-8 text-xl text-gray-500 leading-8">
-            Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At
-            arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae
-            feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget.
-            Eleifend egestas fringilla sapien.
-          </p>
-        </div>
-        <div className="mt-6 prose prose-red prose-lg text-gray-500 mx-auto">
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong>{' '}
-            sed <strong>eget risus enim</strong>. Mattis mauris semper sed amet
-            vitae sed turpis id. Id dolor praesent donec est. Odio penatibus
-            risus viverra tellus varius sit neque erat velit. Faucibus commodo
-            massa rhoncus, volutpat. Dignissim sed eget risus enim.{' '}
-            <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id.
-          </p>
-          <ul role="list">
-            <li>Quis elit egestas venenatis mattis dignissim.</li>
-            <li>
-              Cras cras lobortis vitae vivamus ultricies facilisis tempus.
-            </li>
-            <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-          </ul>
-          <p>
-            Quis semper vulputate aliquam venenatis egestas sagittis quisque
-            orci. Donec commodo sit viverra aliquam porttitor ultrices gravida
-            eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet,
-            mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed
-            elit interdum dignissim.
-          </p>
-          <h2>From beginner to expert in 30 days</h2>
-          <p>
-            Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat
-            in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum
-            mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-            tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
-            Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
-            diam.
-          </p>
-          <blockquote>
-            <p>
-              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
-              urna sed consectetur neque tristique pellentesque. Blandit amet,
-              sed aenean erat arcu morbi.
-            </p>
-          </blockquote>
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit.
-          </p>
-          <figure>
-            <Image
-              className="w-full rounded-lg"
-              src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
-              alt=""
-              width={1310}
-              height={873}
-            />
-            <figcaption>
-              Sagittis scelerisque nulla cursus in enim consectetur quam.
-            </figcaption>
-          </figure>
-          <h2>Everything you need to get up and running</h2>
-          <p>
-            Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>.
-            Amet, massa quam varius orci dapibus volutpat cras. In amet eu
-            ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
-            viverra ridiculus non molestie. Gravida quis fringilla amet eget dui
-            tempor dignissim. Facilisis auctor venenatis varius nunc, congue
-            erat ac. Cras fermentum convallis quam.
-          </p>
-          <p>
-            Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-            enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-            praesent donec est. Odio penatibus risus viverra tellus varius sit
-            neque erat velit.
-          </p>
-        </div>
+      <div className="relative px-4 sm:px-6 lg:px-8 prose lg:prose-xl mx-auto" dangerouslySetInnerHTML={{__html: post.html}}>
+        
       </div>
     </div>
   );
